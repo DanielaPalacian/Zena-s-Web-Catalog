b2377803c0f10126bd768036ea604997e6d13e6d
DanielaPalacian <38717109+DanielaPalacian@users.noreply.github.com>
2026-08-11 14:09:00 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,20 +8,20 @@ from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(f" Zena's Amazing Athleisure Catalog ")
-st.write(
-  """Pick a sweatsuit color or style
-  """
-)
+# st.write(
+  # """Pick a sweatsuit color or style
+  # """
+# )
 
 
 cnx=st.connection("snowflake")
 session= cnx.session()
 
-my_dataframe = session.table("zenas_athleisure_db.products.upsell_mapping").select(col('sweatsuit_color_or_style'))
+my_dataframe = session.table("zenas_athleisure_db.products.catalog_for_website").select(col('sweatsuit_color_or_style'))
 ingredients_list = st.multiselect('Pick a sweatsuit color or style'
     ,my_dataframe
     ,max_selections=1)
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
 pd_df=my_dataframe.to_pandas()
