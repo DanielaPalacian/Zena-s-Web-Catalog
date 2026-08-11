e3b29b88c8e6ab35845dafc48b092170e7289a8b
DanielaPalacian <38717109+DanielaPalacian@users.noreply.github.com>
2026-08-11 15:57:50 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,9 @@ import pandas as pd
 
 st.title("Zena's Amazing Athleisure Catalog")
 
-session = get_active_session()
-
 st.stop()
+
+session = get_active_session()
 
 # get a list of colors for a drop list selection
 table_colors = session.sql("select color_or_style from catalog_for_website")
